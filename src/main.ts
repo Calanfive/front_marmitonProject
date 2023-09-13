e303ab1dbda259ffb1d4f2557d53076098644867
calanfive <camille.lelan@gmail.com>
2023-09-13 17:10:20 +0200

    front1

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,90 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const title = document.createElement('h1');
+title.setAttribute("id","section_top");
+title.innerText = 'Marmitop';
+title.style.textAlign = 'center'
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const section_box1 = document.createElement('div');
+
+const section_inputs = document.createElement('div');
+section_inputs.classList.add('section_inputs');
+
+const input1 = document.createElement('input');
+const input2 = document.createElement('input');
+const input3 = document.createElement('input');
+const input4 = document.createElement('input');
+const label1 = document.createElement('p');
+const label2 = document.createElement('p');
+const label3 = document.createElement('p');
+const label4 = document.createElement('p');
+const button = document.createElement('button');
+
+label1.innerText = 'nom';
+label2.innerText = 'lien image';
+label3.innerText = 'durée';
+label4.innerText = 'note';
+input1.setAttribute('type', 'text');
+input2.setAttribute('type', 'text');
+input3.setAttribute('type', 'text');
+input4.setAttribute('type', 'text');
+
+section_box1.style.border = "1px solid black";
+section_box1.classList.add('section_box1');
+
+button.setAttribute('type', 'button');
+button.innerText = 'Ajouter';
+
+const label_input1 = document.createElement('div') 
+const label_input2 = document.createElement('div')
+const label_input3 = document.createElement('div') 
+const label_input4 = document.createElement('div')
+label_input1.classList.add('label_input');
+label_input2.classList.add('label_input');
+label_input3.classList.add('label_input');
+label_input4.classList.add('label_input');
+
+label_input1.appendChild(label1);
+label_input1.appendChild(input1);
+
+label_input2.appendChild(label2);
+label_input2.appendChild(input2);
+
+label_input3.appendChild(label3);
+label_input3.appendChild(input3);
+
+label_input4.appendChild(label4);
+label_input4.appendChild(input4);
+
+section_inputs.appendChild(label_input1);
+section_inputs.appendChild(label_input2);
+section_inputs.appendChild(label_input3);
+section_inputs.appendChild(label_input4);
+
+section_box1.appendChild(section_inputs);
+
+const bulleButton = document.createElement('div')
+bulleButton.classList.add('bulle-button')
+bulleButton.appendChild(button)
+
+section_box1.appendChild(bulleButton);
+
+const section_title1= document.createElement('h2');
+section_title1.innerText = 'Mes recettes';
+section_title1.classList.add('section_title1');
+
+const section_box2= document.createElement('div');
+section_box2.innerText = 'Les choux à la fraise';
+section_box2.classList.add('section_box2');
+
+const section_box3= document.createElement('div');
+section_box3.innerText = 'Les choux à la crème';
+section_box3.classList.add('section_box3');
+
+const body_page = document.querySelector('#app') as HTMLDivElement;
+
+body_page.appendChild(title)
+body_page.appendChild(section_box1)
+// body_page.appendChild(section_title1)
+// body_page.appendChild(section_box2)
+// body_page.appendChild(section_box3)
